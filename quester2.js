79b87b86a92348d796300f992d4a3e9fab9090e1
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-27 12:09:50 -0300

    Create quester2.js

---
+++ quester2.js
@@ -0,0 +1,16 @@
+let pessoa ={
+    nome: 'Anselmo',
+    idade: 17,
+    Treina: true,
+
+    bodybulder: function() {    // uma das formas de fazer function
+        console.log(Biiiiiirrrrllllll);
+    },
+
+     imprimirNome: function(){
+        return'nome' + this.nome
+     }  
+}
+
+pessoa.imprimirNome()
+console.log(pessoa.imprimirNome_return());
