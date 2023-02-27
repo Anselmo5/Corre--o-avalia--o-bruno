f70fb9e75729649a26c6196e0dfa5ab08e5f0d90
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-27 12:09:47 -0300

    Create quester1.js

---
+++ quester1.js
@@ -0,0 +1,27 @@
+// Atr 1 inicio
+let pessoa ={
+    nome: 'Anselmo',
+    idade: 17,
+    Treina: true,
+
+    bodybulder: function() {    // uma das formas de fazer function
+        console.log(Biiiiiirrrrllllll);
+    },
+
+// estudaremos this (this-este)
+// return - retorna todo o valor da variavel
+
+    soma: function(a,b){
+        let c = a+b
+        console.log(a+'+'+'='+c);
+
+    }
+}
+
+console.log(pessoa.idade);
+pessoa.bodybulder()
+pessoa.soma(16,18)
+
+
+//
+
