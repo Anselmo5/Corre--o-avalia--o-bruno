b8f101d3cd60b5399e5ed9523f10ab08dc33f7fc
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-27 12:09:56 -0300

    Create quester4.js

---
+++ quester4.js
@@ -0,0 +1,13 @@
+let pessoa ={
+    nome: 'Anselmo',
+    salario: 4500
+}
+
+
+let pessoa2 ={
+    nome: 'Bruno',
+    salario: 10000
+}
+
+let mediasalial = (pessoa.salario + pessoa2.salario)/2
+console.log('media salarial' +mediasalial);
